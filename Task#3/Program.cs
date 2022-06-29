824f57ac02f4b335927d4d5bfeb6883b9ef6815b
kirill <pershinkirill.n@gmail.com>
2022-06-29 20:27:02 +0300

    Third task has been done

---
+++ Task#3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 7)
+{
+    Console.WriteLine("A week has only seven days in it");
+}
+else
+{
+    if (number < 6)
+    {
+        Console.WriteLine("It's a workday");
+    }
+    else
+    {
+        Console.WriteLine("Congrats - it's a weekend");
+    }
+}
