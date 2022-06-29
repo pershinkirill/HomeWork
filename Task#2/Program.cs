85965639c44fcd96bc01ceb6c08c9f9a9c20dd7a
kirill <pershinkirill.n@gmail.com>
2022-06-29 20:11:12 +0300

    Second task has been done

---
+++ Task#2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("The number is too low, therefore there is not a third character");
+}
+else
+{
+    string myString = number.ToString(); //a little bit chiterstvo
+    Console.WriteLine($"The number's third character - {myString[2]}");
+
+}
