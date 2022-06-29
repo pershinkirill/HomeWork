42261d820da8b8671dd955a1ebd5f5934d2a00a2
kirill <pershinkirill.n@gmail.com>
2022-06-29 19:20:16 +0300

    first task has been done

--- Task#1/Program.cs
+++ Task#1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(99,999);
+Console.WriteLine($"Random number - {number}");
+
+string stNumber = number.ToString();
+stNumber = stNumber.Remove(1,1);
+
+Console.WriteLine($"The number without a middle character - {stNumber}");
