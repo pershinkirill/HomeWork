12f897599c1cdc9fc5d0b1785af9d30fd997c3a0
kirill <pershinkirill.n@gmail.com>
2022-07-05 17:30:13 +0300

    third task has been completed

---
+++ Task#_23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Enter a number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Cube of number {i} is {Math.Pow(i, 3)}");
+}
