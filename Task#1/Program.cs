ee4fabb91c07af8f271ef0779479dd703ee4c5ee
kirill <pershinkirill.n@gmail.com>
2022-07-05 16:06:39 +0300

    Task 19

--- Task#1/Program.cs
+++ Task#1/Program.cs
@@ -1,20 +1,29 @@
-﻿// Задача 10 : Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 Console.WriteLine("Enter a number");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 999)
+if (number > 99999)
 {
     Console.WriteLine("The number is too high");
 }
 else
 {
-    if (number < 100)
+    if (number < 10000)
     {
         Console.WriteLine("The number is too low");
     }
 
     else
     {
-        Console.WriteLine($"The number's middle character - {(number % 100) / 10}");
+        string myString = number.ToString();
+        if (myString[0] == myString[4] && myString[1] == myString[3])
+        {
+            Console.WriteLine($"The number {myString} is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine($"The number {myString} is not a palindrome");
+        }
+
     }
 }
