9cb4142417c3f3200b11ee30941712e07ad85467
kirill <pershinkirill.n@gmail.com>
2022-07-07 15:29:57 +0300

    second task

---
+++ Task#27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Enter the number A");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+string line = a.ToString();
+
+for (int i = 0; i < line.Length; i++)
+{
+    b += (int)char.GetNumericValue(line[i]);
+}
+
+Console.WriteLine($"summ of A charachters is {b}");
