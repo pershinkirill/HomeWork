4c439d32d9a3d68ed5da4124e879e79210aa170b
kirill <pershinkirill.n@gmail.com>
2022-07-09 20:05:26 +0300

    Second task was done

---
+++ Task#36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int [10];
+
+int size = array.Length;
+int index = 0;
+int evenIndexAmount = 0;
+
+while (index < size)
+{
+    array[index] = new Random().Next(1,10); 
+    index ++;
+}
+
+for (index = 0; index < size; index ++)
+{
+    if (index % 2 == 0 && index > 0) evenIndexAmount += array[index];
+}
+Console.WriteLine($"The total sum of elements with even indexes in the aray - [{String.Join("; ", array)}] is {evenIndexAmount} ");
