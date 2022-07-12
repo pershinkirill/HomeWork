a38f28e9f2660d168892f8c198b05e8107f87e44
kirill <pershinkirill.n@gmail.com>
2022-07-12 05:12:37 +0300

    task was corrected

--- Taskr#41/Program.cs
+++ Taskr#41/Program.cs
@@ -2,9 +2,9 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Enter a sequence of numbers, separated with spaces and commas");
+Console.WriteLine("Enter a sequence of numbers, separated with spaces without commas");
 
-string[] numbersSequence = Console.ReadLine().Split(',');
+string[] numbersSequence = Console.ReadLine().Split(' ');
 int counter = 0;
 
 for (int i = 0; i < numbersSequence.Length; i++)
@@ -12,6 +12,6 @@ for (int i = 0; i < numbersSequence.Length; i++)
     if (Convert.ToInt32(numbersSequence[i]) > 0) counter ++;
 }
 
-if (counter > 1) Console.WriteLine($" There are {counter} positive numbers in the array - [{String.Join(";", numbersSequence)}]");
-if (counter == 1) Console.WriteLine($"There is the only 1 number that more than 0 in the array - [{String.Join(";", numbersSequence)}]");
-if (counter == 0) Console.WriteLine($"There is not a single positive number in the array - [{String.Join(";", numbersSequence)}]");
+if (counter > 1) Console.WriteLine($"There are {counter} positive numbers in the array - [{String.Join("; ", numbersSequence)}]");
+if (counter == 1) Console.WriteLine($"There is the only 1 number that more than 0 in the array - [{String.Join("; ", numbersSequence)}]");
+if (counter == 0) Console.WriteLine($"There is not a single positive number in the array - [{String.Join("; ", numbersSequence)}]");
