3884876ae8c11cbe9c7582586b557a7bab3618b4
kirill <pershinkirill.n@gmail.com>
2022-07-18 20:27:16 +0300

    Task#52 has been completed

---
+++ Task#52/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+//there aren't special requirements for the matrix, therefore we can generate it randomly
+int rows = new Random().Next(2,8);
+int columns = new Random().Next(2,8);
+double[] avg = new double [columns]; //Additional array for average values
+
+double[,] matrix = new double [rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        Console.Write(matrix[i,j] + "\t ");
+   
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < columns; j++)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        avg[j] += Math.Round((matrix[i,j] / rows), 2);
+    }
+}
+Console.WriteLine($"The columns average values are - [{String.Join(" ", avg.Select(avg => Math.Round(avg, 2)).ToList())}]");
