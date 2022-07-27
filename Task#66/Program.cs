7f91b1ff6108ff4940f45012cca2f1bc4dd73736
kirill <pershinkirill.n@gmail.com>
2022-07-27 19:11:41 +0300

    Task #66 done

---
+++ Task#66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int ArithmeticProgressionSum(int m, int n)
+{
+    if (m == n) return n;
+    return ArithmeticProgressionSum(m, n - 1) + n;
+}
+
+Console.WriteLine("Please enter M number");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter N number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {m}; N = {n} ->  {ArithmeticProgressionSum(m, n)}");
